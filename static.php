4bf146706539df65f9a8f983f737a12a1d4af3bf
sandeep.jain <sandeep.jain@indianexpress.com>
2020-05-16 16:28:59 +0530

    add files

--- static.php
+++ static.php
@@ -5,12 +5,15 @@ ini_set('display_errors',1);
 class Demo{
 	public static $name;
 	public static function getName(){
-		echo self:: $name;
+		if ( ! empty( self:: $name ) ) {
+			echo sprintf('<h1>%s</h1>', self:: $name );
+		}
 	}
 	public static function setName($a){
 		self:: $name = $a;
 	}
 }
+Demo:: getName();
 Demo:: setName('sandeep');
 Demo:: getName();
 ?>
